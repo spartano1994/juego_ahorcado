987a7f18847a2b5681d42ffd245684d75b708e66
Gerardo González García <spartano1994@hotmail.com>
2024-06-24 19:13:44 -0600

    Construyendo funciones y clases

---
+++ main.py
@@ -0,0 +1,26 @@
+import random as rn
+import time
+
+def run():
+    nivel = menu()
+
+
+def importar_palabras( nivel ):
+    return []
+
+def seleccionar_palabra():
+    return
+
+def menu():
+    pass
+
+class PalabraSecreta( object ) :
+    def __init__( self  , palabra , palabra_oculta ):
+        palabra.self = palabra
+        palabra_oculta.self = __cubrir_palabra()
+
+    def __cubrir_palabra( self ) :
+        pass
+
+if __name__ == "__main__":
+    run()
